21affc6e5e7456b91d388f171af8ca3d65f5e8ce
Sveta <svetlanast0@inbox.ru>
2022-09-12 20:19:52 +0400

    csharp hw6 added

--- seminars/csharp/hw6/Program.cs
+++ seminars/csharp/hw6/Program.cs
@@ -1,16 +1,34 @@
 ﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-
-Console.Write("Input m integer numbers: ");
-int num = Convert.ToInt32(Console.ReadLine());
+/*
+Console.Write("Input quantiti of numbers: ");
+int m = Convert.ToInt32(Console.ReadLine());
 int count = 0;
 
+for (int i = 1; i <= m; i++)
 {
-    while (num <= m)
-    {
-        Console.Write("Input next number: ");
-        count ++;
-    }
-    
+    Console.Write($"Input number {i}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0)
+        count++;
 }
 
-Console.WriteLine($"Count of pozitiv numbers is {count}");
+Console.WriteLine($"Count of positive numbers is {count}");
+*/
+
+//Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*
+Console.Write("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Cross point is ({x};{y})");
+*/
+
