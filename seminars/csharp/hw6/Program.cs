4aa31a2d12c05a65c09a0b005b9650302b70fc66
Sveta <svetlanast0@inbox.ru>
2022-09-12 20:08:36 +0400

    csharp hw6 added

---
+++ seminars/csharp/hw6/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Input m integer numbers: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+{
+    while (num <= m)
+    {
+        Console.Write("Input next number: ");
+        count ++;
+    }
+    
+}
+
+Console.WriteLine($"Count of pozitiv numbers is {count}");
