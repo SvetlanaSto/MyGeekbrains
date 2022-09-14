16d2f20f0751ecde3a734455e97e507b71a04645
Sveta <svetlanast0@inbox.ru>
2022-09-15 00:28:33 +0400

    csharp hw7 added

---
+++ seminars/csharp/hw7/Program.cs
@@ -0,0 +1,139 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+/*
+double[,] CreateRandom2dArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    double[,] newArray = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(0, 1000) + new Random().NextDouble();
+
+    return newArray;
+}
+
+void Show2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+double[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+*/
+
+// Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(0, 1000);
+
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ShowElementByPosition(int[,] array)
+{
+    Console.Write("Input row number: ");
+    int row = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input column number: ");
+    int column = Convert.ToInt32(Console.ReadLine());
+
+    if (row < array.GetLength(0) & column < array.GetLength(1))
+        Console.WriteLine(array[row, column]);
+    else
+        Console.WriteLine("no such element");
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+ShowElementByPosition(myArray);
+*/
+
+// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(0, 1000);
+
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[] GetAveragesOfColumns(int[,] array)
+{
+    double[] resultArray = new double[array.GetLength(1)];
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+            sum += array[i, j];
+        resultArray[j] = sum / array.GetLength(0);
+    }
+
+    return resultArray;
+}
+
+void ShowArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+double[] averages = GetAveragesOfColumns(myArray);
+ShowArray(averages);
+*/
