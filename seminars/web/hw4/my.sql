40cee5e58af9d5344d6aa4a1365d004103e06f19
Sveta <svetlanast0@inbox.ru>
2022-12-28 19:38:37 +0400

    web hw4 (fixes after review)

--- seminars/web/hw4/my.sql
+++ seminars/web/hw4/my.sql
@@ -1,5 +1,5 @@
 -- create
-CREATE TABLE GROUPMATES (
+CREATE TABLE groupmates (
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   age INTEGER NOT NULL,
@@ -7,17 +7,17 @@ CREATE TABLE GROUPMATES (
 );
 
 -- insert
-INSERT INTO GROUPMATES VALUES (1, 'Наталья', '30', 'Москва');
-INSERT INTO GROUPMATES VALUES (2, 'Антон', '29', 'Москва');
-INSERT INTO GROUPMATES VALUES (3, 'Арсений', '26', 'Улан-Удэ');
-INSERT INTO GROUPMATES VALUES (4, 'Андрей', '40', 'Мытищи');
-INSERT INTO GROUPMATES VALUES (5, 'Дарья', '30', 'Саратов');
-INSERT INTO GROUPMATES VALUES (6, 'Дмитрий', '40', 'Балашиха');
-INSERT INTO GROUPMATES VALUES (7, 'Екатерина', '25', 'Санкт-Петербург');
-INSERT INTO GROUPMATES VALUES (8, 'Марина', '31', 'Нефтеюганск');
-INSERT INTO GROUPMATES VALUES (9, 'Ярослав', '18', 'Москва');
-INSERT INTO GROUPMATES VALUES (10, 'Денис', '30', 'Новосибирск');
+INSERT INTO groupmates VALUES (1, 'Наталья', 30, 'Москва');
+INSERT INTO groupmates VALUES (2, 'Антон', 29, 'Москва');
+INSERT INTO groupmates VALUES (3, 'Арсений', 26, 'Улан-Удэ');
+INSERT INTO groupmates VALUES (4, 'Андрей', 40, 'Мытищи');
+INSERT INTO groupmates VALUES (5, 'Дарья', 30, 'Саратов');
+INSERT INTO groupmates VALUES (6, 'Дмитрий', 40, 'Балашиха');
+INSERT INTO groupmates VALUES (7, 'Екатерина', 25, 'Санкт-Петербург');
+INSERT INTO groupmates VALUES (8, 'Марина', 31, 'Нефтеюганск');
+INSERT INTO groupmates VALUES (9, 'Ярослав', 18, 'Москва');
+INSERT INTO groupmates VALUES (10, 'Денис', 30, 'Новосибирск');
 
 -- fetch 
-SELECT name FROM GROUPMATES WHERE address = 'Москва' AND age BETWEEN 18 AND 29;
+SELECT name FROM groupmates WHERE address = 'Москва' AND age BETWEEN 18 AND 29;
 
